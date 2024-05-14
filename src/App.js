c47fe2a06fa447ae611f0d782d16c1a853df1319
fatemahalzahraa <fatemah.a.baqer@gmail.com>
2024-05-14 18:39:49 +0300

    button task

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const addOne = () => {
+    setCounter(counter + 1);
+  };
+  //why did we put h1 of counter?
+  //setCounter why did we use brackets and not = ????
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>{counter}</h1>
+      <button onClick={addOne}>Click Me!</button>
+    </div>
+  );
+}
+
+export default App;
